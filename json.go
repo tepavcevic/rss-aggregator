e0d51a85cc55a2825be359bde97f8ed495a10486
tepavcevic <djordje.tepa@gmail.com>
2023-09-24 16:38:59 +0200

    Add JSON and error handlers

--- json.go
+++ json.go
@@ -6,7 +6,21 @@ import (
 	"net/http"
 )
 
-func respondWithJSoN(w http.ResponseWriter, code int, payload interface{}) {
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Println("Error:", msg)
+	}
+
+	type errResponse struct {
+		Error string `json:"error"`
+	}
+
+	respondWithJSON(w, code, errResponse{
+		Error: msg,
+	})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	data, err := json.Marshal(payload)
 	if err != nil {
 		log.Printf("Failed to marshal json response %v", payload)
